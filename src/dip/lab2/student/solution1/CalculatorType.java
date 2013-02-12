8250e0c2e01d0463a82f115536b8a5b0f4d0959d
Michael <Michael@Control>
2013-02-11 20:15:50 -0600

    Started Interface

---
+++ src/dip/lab2/student/solution1/CalculatorType.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2.student.solution1;
+
+/**
+ *
+ * @author Michael
+ */
+public interface CalculatorType {
+    static final double GOOD_RATE = 0.20;
+    static final double FAIR_RATE = 0.15;
+    static final double POOR_RATE = 0.10;
+
+    double getTip();
+
+    public void setServiceRating();
+
+    public static class ServiceQuality {
+    
+    }
+    public ServiceQuality getServiceQuality();
+    
+    public void setServiceRating(ServiceQuality q);
+    
+}
--- src/dip/lab2/student/solution1/FoodServiceTipCalculator.java
+++ src/dip/lab2/student/solution1/FoodServiceTipCalculator.java
@@ -8,7 +8,7 @@ package dip.lab2.student.solution1;
  *
  * @author Michael Grinker
  */
-public class FoodServiceTipCalculator {
+public class FoodServiceTipCalculator implements CalculatorType {
     private static final double MIN_BILL = 0.00;
     private static final String BILL_ENTRY_ERR =
             "Error: bill must be greater than or equal to " + MIN_BILL;
@@ -27,6 +27,7 @@ public class FoodServiceTipCalculator {
         this.setBill(billAmt);
     }
 
+    @Override
     public double getTip() {
         double tip = 0.00; // always initialize local variables
 
@@ -52,11 +53,13 @@ public class FoodServiceTipCalculator {
         bill = billAmt;
     }
 
+    @Override
     public final void setServiceRating(ServiceQuality q) {
         // No need to validate because enums provide type safety!
         serviceQuality = q;
     }
 
+    @Override
     public ServiceQuality getServiceQuality() {
         return serviceQuality;
     }
