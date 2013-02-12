5ee7ccb1a6cafb3f0d15bb2bd9e128634d00ee98
Michael <Michael@Control>
2013-02-12 12:39:27 -0600

    Problems with object arguments

--- src/dip/lab2/student/solution1/ProgramStart.java
+++ src/dip/lab2/student/solution1/ProgramStart.java
@@ -12,6 +12,9 @@ public class ProgramStart {
     public static void main(String[] args) {
         
          CalculatorType foodTip = 
-                new FoodServiceTipCalculator();
+                new FoodServiceTipCalculator
+                 (FoodServiceTipCalculator.ServiceQuality.GOOD, 25.25);
+         TipCalculator newTip = new TipCalculator(tip);
+         
     }
 }
