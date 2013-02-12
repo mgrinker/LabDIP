1a0c5cb9cb1345cef8d36acd4828e766d89fe811
Michael <Michael@Control>
2013-02-12 12:15:07 -0600

    Created program start

---
+++ src/dip/lab2/student/solution1/ProgramStart.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2.student.solution1;
+
+/**
+ *
+ * @author Michael
+ */
+public class ProgramStart {
+    public static void main(String[] args) {
+        
+    }
+}
