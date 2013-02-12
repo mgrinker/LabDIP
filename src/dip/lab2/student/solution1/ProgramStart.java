01d08785c7b2dda8ccb9ce87a26ed2fc191324ec
Michael <Michael@Control>
2013-02-12 13:15:50 -0600

    Successfully calculated bag service tip

--- src/dip/lab2/student/solution1/ProgramStart.java
+++ src/dip/lab2/student/solution1/ProgramStart.java
@@ -5,6 +5,7 @@
 package dip.lab2.student.solution1;
 import java.text.DecimalFormat;
 
+
 /**
  *
  * @author Michael
@@ -12,6 +13,7 @@ import java.text.DecimalFormat;
 public class ProgramStart {
     public static void main(String[] args) {
         
+              
         // Create decimal format object
         DecimalFormat dollar = new DecimalFormat("#,##0.00");
         
@@ -19,11 +21,23 @@ public class ProgramStart {
         CalculatorType foodTip = 
                 new FoodServiceTipCalculator
                  (FoodServiceTipCalculator.ServiceQuality.GOOD, 25.25);
+        
          TipCalculator newTip = new TipCalculator(foodTip);
          newTip.getTip();
+         
          System.out.println("Your food service tip is $"
                  + dollar.format(foodTip.getTip()));
          
+         // Get a baggage service tip
+         CalculatorType bagTip = new BaggageServiceTipCalculator
+                 (BaggageServiceTipCalculator.ServiceQuality.FAIR,5);
+         
+         TipCalculator newBagTip = new TipCalculator(bagTip);
+
+         newBagTip.getTip();
+         
+         System.out.println("Your baggage tip is $"
+                 + dollar.format(bagTip.getTip()));
          
     }
 }
